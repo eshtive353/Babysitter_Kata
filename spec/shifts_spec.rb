5a63d50594b6aeee7603b52690978c94e15d0961
Eric Shtivelberg <eshtive353@gmail.com>
2015-10-05 15:23:50 -0400

    Consolidate tests

--- spec/shifts_spec.rb
+++ spec/shifts_spec.rb
@@ -2,57 +2,38 @@ require_relative '../lib/shifts'
 
 describe Shifts do
 
-  it 'any start time before 5pm (17:00) is invalid' do
-    night = Shifts.new
-    expect(night.start_time(12)).to eq("That start time is too early!")
-  end
-
   it 'a valid start time is saved' do
     night = Shifts.new
     expect(night.start_time(17)).to eq(17)
-  end
-
-  it 'any end time after 4am (4:00) is invalid' do
     night = Shifts.new
-    expect(night.end_time(5)).to eq("That end time is too late!")
+    expect(night.start_time(12)).to eq("That start time is too early!")
   end
 
   it 'a valid end time is saved' do
     night = Shifts.new
     expect(night.end_time(4)).to eq(4)
+    night = Shifts.new
+    expect(night.end_time(5)).to eq("That end time is too late!")
   end
 
-  it 'a bedtime later than midnight is too late for the kid' do
+  it 'a bedtime is set, with a cutoff at midnight and with a bedtime of midnight equaling 24 for calculations' do
+    night = Shifts.new
+    expect(night.bedtime(0)).to eq(24)
+    night = Shifts.new
+    expect(night.bedtime(22)).to eq(22)
     night = Shifts.new
     expect(night.bedtime(0.5)).to eq('Are you crazy? Your child needs some sleep!')
   end
 
-  it 'a bedtime of midnight returns a value of 24 for calculation reasons' do
-    night = Shifts.new
-    expect(night.bedtime(0)).to eq(24)
-  end
-
-  it 'any other bedtime is saved as is' do
-    night = Shifts.new
-    expect(night.bedtime(22)).to eq(22)
-  end
-
-  it 'the first shift is calculated' do
+  it 'the first shift is calculated, with correct rounding' do
     night = Shifts.new
     night.start_time(17)
     night.bedtime(22)
     expect(night.first_shift).to eq(5)
-  end
-
-  it 'if the babysitter works less than a quarter of an hour for first shift, rounds down' do
     night = Shifts.new
     night.start_time(17)
     night.bedtime(22.1)
     expect(night.first_shift).to eq(5)
-  end
-
-  it 'if the babysitter works a quarter of an hour or more for first shift, rounds up' do
-    night = Shifts.new
     night.start_time(17)
     night.bedtime(22.25)
     expect(night.first_shift).to eq(6)
